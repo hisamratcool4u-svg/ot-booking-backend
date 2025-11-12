005275cef1a6ab1bec4f9b6be66080f757781127
hisamratcool4u-svg <hisamratcool4u@gmail.com>
2025-11-12 16:00:15 +0530

    updated docker file
    updated docker file

---
+++ ot-backend-updated/src/main/java/com/otapp/repository/OperationTheatreRepository.java
@@ -0,0 +1,7 @@
+package com.otapp.repository;
+
+import com.otapp.entity.OperationTheatre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OperationTheatreRepository extends JpaRepository<OperationTheatre, Long> {
+}
